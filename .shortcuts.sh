8249a050912af7b4f9ae32eae01d9433cbee4772
cokelly <okellyconor@gmail.com>
2016-03-30 15:20:47 +0100

    First commit with current versions of files

---
+++ .shortcuts.sh
@@ -0,0 +1,40 @@
+
+# Easier navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Shortcuts - Navigation
+alias d="cd ~/Desktop"
+alias dr="cd /Users/cokelly/Google_Drive"
+alias c="cd /Users/cokelly/Google_Drive/Code"
+alias ucd="cd /Users/cokelly/Google_Drive/1._UCD_conversion_masters/1._Term_2"
+alias cucd="cd /Users/cokelly/Google_Drive/1._UCD_conversion_masters/1._Term_2/Programming_2"
+alias o="open ."
+alias mk="mkdir"
+alias down="cd /Users/cokelly/Desktop/Donwloads; rtorrent"
+alias sdown="screen;cd /Users/cokelly/Desktop/Donwloads; rtorrent"
+
+# Git shortcuts
+alias gs='git status '
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit -m'
+alias gd='git diff'
+alias go='git checkout '
+alias gk='gitk --all&'
+alias gx='gitx --all'
+alias gp='git push'
+
+
+alias got='git '
+alias get='git '
+
+# Shortcuts - Files
+alias hosts='sudo nano /etc/hosts'
+
+# Jokes
+alias dance="echo I do not dance, bitch"				
+alias moo="cowsay Not all cows moo you know"
+alias begin="cmatrix"
