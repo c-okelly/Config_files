1fe231ea7efb3164b200f5ccd4f246c6701f1701
cokelly <okellyconor@gmail.com>
2016-10-12 14:34:41 +0100

    Updated repo

---
+++ .shortcuts.sh
@@ -0,0 +1,41 @@
+
+# Easier navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Shortcuts - Navigation
+alias d="cd ~/Desktop"
+alias dr="cd /Users/cokelly/Google_Drive"
+alias c="cd /Users/cokelly/Google_Drive/Code"
+alias ucd="cd /Users/cokelly/Google_Drive/1._UCD_conversion_masters/1._Term_4"
+alias o="open ."
+alias mk="mkdir"
+alias down="cd /Users/cokelly/Desktop/Downloads; rtorrent"
+alias ddown="mv /Users/cokelly/Desktop/*.torrent /Volumes/Storage/Downloads; cd /Volumes/Storage/Downloads; rtorrent"
+alias aws="ssh -i "/Users/cokelly/.ssh/First_instance.pem" ec2-user@ec2-52-49-92-149.eu-west-1.compute.amazonaws.com"
+alias karabiner="cd /Users/cokelly/Library/Application\ Support/Karabiner"
+
+# Git shortcuts
+alias gs='git status '
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit -m'
+alias gd='git diff'
+alias go='git checkout '
+alias gk='gitk --all&'
+alias gx='gitx --all'
+alias gp='git push'
+
+
+alias got='git '
+alias get='git '
+
+# Shortcuts - Files
+alias hosts='sudo nano /etc/hosts'
+
+# Jokes
+alias dance="echo I do not dance, bitch"				
+alias moo="cowsay Not all cows moo you know"
+alias begin="cmatrix"
